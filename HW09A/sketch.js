d322716ab33149ce0c668ab2bff593bd0e345c9a
Lauren Tsao <et2676@nyu.edu>
2024-11-11 12:43:47 -0500

    cleaned up HW09A

--- HW09A/sketch.js
+++ HW09A/sketch.js
@@ -1,9 +1,10 @@
-// Original image, to use as reference for pixel colors
+// original image (Mondrian) - to use as reference for pixel colors
 let oImg;
 
-// Display image, to modify and display on canvas
+// my image (Mondrian) - to modify and display on canvas
 let mImg;
 
+// my Gif array
 let mGif = [];
 
 let rSlider;
@@ -13,7 +14,9 @@ let GifSlider;
 
 let colorPicker;
 
+
 function preload() {
+  // loading images and Gif frames
   oImg = loadImage("../assets/mondriaan.jpg");
   mImg = loadImage("../assets/mondriaan.jpg");
   
@@ -22,24 +25,26 @@ function preload() {
   }
 }
 
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   oImg.resize(0, height);
   mImg.resize(0, height);
 
-  colorPicker = createColorPicker('#003f7c');
+  // creating color picker
+  colorPicker = createColorPicker('#003f7c'); // starting color: Mondrian blue
   colorPicker.position(200, 212);
 
-  // Resize images in mGif array
+  // resizing each Gif in array
   for (let i = 0; i < mGif.length; i++) {
     mGif[i].resize(0, 330);
   }
 
-  // Load pixels after resizing images
+  // loading pixels after resizing images
   oImg.loadPixels();
   mImg.loadPixels();
 
-  // Setup sliders
+  // creating sliders
   rSlider = createSlider(0, 255, 253);
   rSlider.position(100, 140);
 
@@ -52,7 +57,7 @@ function setup() {
   GifSlider = createSlider(0, 45, 0);
   GifSlider.position(100, 270);
 
-  // Set slider styles once in setup
+  // slider styles
   rSlider.style('width', '150px');
   rSlider.style('height', '3px');
 
@@ -67,50 +72,61 @@ function setup() {
 }
 
 function draw() {
-  // Modify mImg pixels
+  // load mImg for modification
   mImg.loadPixels();
 
-  let pickedColor = colorPicker.color();  // get the picked color
-  let pickedR = red(pickedColor);         // extract red component
-  let pickedG = green(pickedColor);       // extract green component
+  let pickedColor = colorPicker.color();
+  let pickedR = red(pickedColor);
+  let pickedG = green(pickedColor);
   let pickedB = blue(pickedColor);
 
-  // Get slider values
+  // get slider values
   let rAmount = rSlider.value();
   let gAmount = gSlider.value();
   let bAmount = bSlider.value();
   let GifFrame = GifSlider.value();
 
-  // Iterate through pixels of oImg and modify mImg
+  // cycling through the RGBA pixels of oImg to prepare for mImg modification
   for (let idx = 0; idx < oImg.pixels.length; idx += 4) {
     let redVal = oImg.pixels[idx + 0];
     let greenVal = oImg.pixels[idx + 1];
     let blueVal = oImg.pixels[idx + 2];
     let alphaVal = oImg.pixels[idx + 3];
 
+    // SETTING COLOR THRESHOLDS
+    // Mondrian Red: (253, 67, 42)
     let pixelIsRed = redVal > 2 * greenVal && redVal > 2 * blueVal && redVal > 200;
+
+    // Mondrian Blue: (3, 69, 129)
     let pixelIsBlue = blueVal > 2 * redVal && blueVal > 1 * greenVal && blueVal > 1;
+
+    // Mondrian Yellow (255, 211, 92)
     let pixelIsYellow = redVal > 1 * greenVal && redVal > 2 * blueVal && redVal > 200;
 
-    // Modify pixel colors based on conditions
+    // MODIFYING PIXEL COLORS OF mImg BASED ON ABOVE THRESHOLDS
+    // Mondrian red areas will be modified by silder values 0-255
     if (pixelIsRed) {
       mImg.pixels[idx + 0] = rAmount;
       mImg.pixels[idx + 1] = gAmount;
       mImg.pixels[idx + 2] = bAmount;
       mImg.pixels[idx + 3] = alphaVal;
     }
+    // Mondrian blue areas will be modified by color picker
     else if (pixelIsBlue) {
       mImg.pixels[idx + 0] = pickedR;
       mImg.pixels[idx + 1] = pickedG;
       mImg.pixels[idx + 2] = pickedB;
       mImg.pixels[idx + 3] = alphaVal;
     }
+    // Mondrian yellow areas will be transparent
     else if (pixelIsYellow) {
       mImg.pixels[idx + 0] = 0;
       mImg.pixels[idx + 1] = 0;
       mImg.pixels[idx + 2] = 0;
       mImg.pixels[idx + 3] = 0;
-    } else {
+    }
+    // for everything else
+    else {
       mImg.pixels[idx + 0] = redVal;
       mImg.pixels[idx + 1] = greenVal;
       mImg.pixels[idx + 2] = blueVal;
@@ -118,18 +134,21 @@ function draw() {
     }
   }
 
+  // update mImg with above modifications
   mImg.updatePixels();
 
-  // Display images
+  // display results after modification
   image(oImg, 0, 0);
+
   push();
   blendMode(HARD_LIGHT);
   image(mGif[GifFrame], 0, 500);
   image(mGif[GifFrame], 325, 0); 
   pop();
+  
   image(mImg, 0, 0);
 
-  // Display slider values
+  // displaying slider values
   fill(255);
   textSize(14);
   text("Red Amount: " + rAmount, 260, 148);
